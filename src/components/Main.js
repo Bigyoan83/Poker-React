8a7580771c08da29735a38996a80cb4f2ef8c36f
Bigyoan83 <163867092+Bigyoan83@users.noreply.github.com>
2025-01-07 07:53:02 +0100

    main 5 cartes

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,17 +1,38 @@
-import '../style/Main.css'
-import {toutesLesCartes} from './CardList'
+import '../style/Main.css';
+import { toutesLesCartes } from './CardList';
+import { useState } from 'react';
 
-function Main () {
+function Main() {
+    const [cartesRestantes, setCartesRestantes] = useState(toutesLesCartes);
+    const [main, setMain] = useState([]);
 
-    const carteAleatoire = toutesLesCartes[Math.floor(Math.random() * toutesLesCartes.length)];
-    console.log(carteAleatoire);
+    const tirerUneCarte = () => {
+        if (cartesRestantes.length > 0) {
+            const indexAleatoire = Math.floor(Math.random() * cartesRestantes.length);
+            const nouvelleCarte = cartesRestantes[indexAleatoire];
+
+            
+            if (main.length < 5) {
+                setMain([...main, nouvelleCarte]);
+                setCartesRestantes(cartesRestantes.filter((_, index) => index !== indexAleatoire));
+            } else {
+                alert("Main pleine");
+            }
+        } else {
+            alert("Toutes les cartes ont été tirées !");
+        }
+    };
 
     return (
-    <div className='main'>
-        <h2>Main du joueur 1</h2>
-        <p>{carteAleatoire}</p>
-    </div>
-    )
+        <div className='main'>
+            <h2>Main du joueur 1</h2>
+            <p className='carte-main'>
+                {main.length > 0 ? main.join('/') : "Aucune carte tirée pour l'instant"}
+            </p>
+            <button onClick={tirerUneCarte}>Tirer une autre carte</button>
+        </div>
+    );
 }
 
 export default Main;
+
